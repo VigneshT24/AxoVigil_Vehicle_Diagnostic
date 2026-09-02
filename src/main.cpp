0588429803859b3dc12dde4969db51071fb4b2e5
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2026-09-01 23:35:01 -0700

    started writing the logic for main cpp and updated cmakelist to include ftxui package

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <sstream>
 #include <thread>
 #include <chrono>
+#include <random>
+#include <unordered_map>
 
 #include <AccelerometerSensor.hpp>
 #include <BatterySensor.hpp>
@@ -14,6 +16,15 @@
 #include <TirePressureSensor.hpp>
 
 // our vehicle diagnostic system will utilize a trapezoidal velocity profile testing methodology
+
+double randomDouble(double min, double max) {
+    static std::random_device rd;
+    static std::mt19937 generator(rd());
+
+    std::uniform_real_distribution<double> distribution(min, max);
+
+    return distribution(generator);
+}
 
 int main() {
     std::cout << "================================================= AxoVigil Vehical Diagnostic =================================================" << std::endl;
@@ -38,25 +49,114 @@ int main() {
 
     std::cout << "The test duration: " << (test_duration / 60) << min_or_mins << "and " << (test_duration % 60) << sec_or_secs << std::endl;
 
-    int max = test_duration;
-    while (test_duration != 0) {
-        std::cout << "\r\033[2K";
+    std::cout << "I need some information about your vehicle." << std::endl << std::endl;
 
-        if (test_duration <= max && test_duration > (max * (2.0/3.0))) {
-            std::cout << "[ACCELERATING] | Time Remaining: " << test_duration << " seconds.";
-        }
-        else if (test_duration <= (max * (2.0/3.0)) && test_duration > (max * (1.0/3.0))) {
-            std::cout << "[CRUSING] | Time Remaining: " << test_duration << " seconds.";
-        }
-        else {
-            std::cout << "[DECELERATING] | Time Remaining: " << test_duration << " seconds.";
-        }
-        std::cout << std::flush;
+    std::unordered_map<std::string, int> vehicleType;
+    double fuelRemain;
 
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Please enter the number corresponding to your vehicle's type: " << std::endl
+              << "0: Sedan" << std::endl 
+              << "1: SUV" << std::endl
+              << "2: Hatchback" << std::endl
+              << "3: Coupe" << std::endl
+              << "4: Convertible" << std::endl
+              << "5: Pickup Truck" << std::endl;
+    
+    std::cout << "Enter: ";
 
-        test_duration--;
+    int temp_num;
+    std::cin >> temp_num;
+
+    while (temp_num < 0 || temp_num > 5) {
+        std::cout << "\nPlease enter an integer between 0 and 5, corresponding to your vehicle type." << std::endl;
+        std::cout << "Enter: ";
+        std::cin >> temp_num;
     }
+
+    std::cout << "\nGot it. Now please enter your vehicle's current fuel remaining (a number between 0.0 and 100.0, as percentage, no percent sign)." << std::endl;
+    std::cout << "Enter: ";
+    std::cin >> fuelRemain;
+
+    while (fuelRemain < 0.0 || fuelRemain > 100.0) {
+        std::cout << "\nPlease enter an integer between 0.0 and 100.0, corresponding to the fuel percentage of your vehicle currently." << std::endl;
+        std::cout << "Enter: ";
+        std::cin >> fuelRemain;
+    }
+    
+    switch(temp_num) {
+        case 0:
+            vehicleType["Sedan"] = 55;
+            break;
+        case 1:
+            vehicleType["SUV"] = 65;
+            break;
+        case 2:
+            vehicleType["Hatchback"] = 45;
+            break;
+        case 3:
+            vehicleType["Coupe"] = 60;
+            break;
+        case 4:
+            vehicleType["Convertible"] = 55;
+            break;
+        case 5:
+            vehicleType["Pickup Truck"] = 98;
+            break;
+    }
+
+    std::cout << "\n\nJust to confirm, your vehicle type is a " << vehicleType.begin()->first << "." << std::endl
+              << "You have currently " << (fuelRemain / 100.0) * vehicleType.begin()->second << " liters of fuel out of " << vehicleType.begin()->second << " total liters." << std::endl; 
+
+    VehicleSpeedSensor speedSensor;
+    VehicleAccelerometerSensor accelSensor;
+    VehicleBatterySensor battSensor;
+    VehicleFuelSensor fuelSensor;
+    VehicleBrakePressureSensor brakeSensor;
+    VehicleTemperatureSensor tempSensor;
+    VehicleTirePressureSensor tireSensor;
+    
+    // setting initial values before phase 1
+    // values are chosen based on research on average estimated sensor reading of an idle vehicle
+    speedSensor.setSpeed(0.0);
+
+    accelSensor.setBias(randomDouble(-0.15, 0.15), randomDouble(-0.15, 0.15), randomDouble(-0.15, 0.15));
+    accelSensor.setNoiseLevel(randomDouble(0.005, 0.030));
+    accelSensor.setAcceleration(0.0, 0.0, 9.80665);
+
+    battSensor.setVoltage(randomDouble(13.7, 14.5));
+    battSensor.setCurrent(randomDouble(1.0, 15.0));
+    battSensor.setTemperature(randomDouble(15, 38));
+    battSensor.setVoltageBias(randomDouble(-0.03, 0.03));
+    battSensor.setCurrentBias(randomDouble(-0.10, 0.10));
+    battSensor.setTemperatureBias(randomDouble(-3.0, 3.0));
+    battSensor.setNoiseLevel(randomDouble(0.005, 0.025));
+
+    fuelSensor.setTankCapacity(70); // this one depends on the type of vehicle (prompted prior)
+    fuelSensor.setFuelLiters(38.5); // this one depends on the fuel set by user (prompted prior)
+    fuelSensor.setBias(randomDouble(-0.50, 0.50));
+    fuelSensor.setSloshNoise(randomDouble(0.0, 0.05));
+
+    brakeSensor.setPressureBias(randomDouble(-5.0, 5.0));
+    brakeSensor.setNoiseLevel(randomDouble(0.25, 2.0));
+    brakeSensor.setFrontBrakePressure(randomDouble(0.0, 2.0));
+    brakeSensor.setRearBrakePressure(randomDouble(0.0, 2.0));
+
+    tempSensor.setAmbientTemperature(randomDouble(18.0, 30.0));
+    tempSensor.setCoolantTemperature(randomDouble(91.0, 98.0));
+    tempSensor.setBias(randomDouble(-0.4, 0.4));
+    tempSensor.setNoiseLevel(randomDouble(0.02, 0.15));
+
+    tireSensor.setFrontLeftPressure(randomDouble(34.0, 36.0));
+    tireSensor.setFrontRightPressure(randomDouble(34.0, 36.0));
+    tireSensor.setRearLeftPressure(randomDouble(34.0, 36.0));
+    tireSensor.setRearRightPressure(randomDouble(34.0, 36.0));
+    tireSensor.setFrontLeftTemperature(randomDouble(16.5, 31.5));
+    tireSensor.setFrontRightTemperature(randomDouble(16.5, 31.5));
+    tireSensor.setRearLeftTemperature(randomDouble(16.5, 31.5));
+    tireSensor.setRearRightTemperature(randomDouble(16.5, 31.5));
+    tireSensor.setPressureBias(randomDouble(-0.5, 0.5));
+    tireSensor.setPressureNoise(randomDouble(0.02, 0.10));
+
     std::cout << std::endl;
     return 0;
 }
